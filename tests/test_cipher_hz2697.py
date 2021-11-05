a6c195b25cbbc301d622c5161cba4220e6c1d3bf
hanyu_zhang <hannahzhang2020@163.com>
2021-11-05 14:39:17 -0400

    add test.py

--- tests/test_cipher_hz2697.py
+++ tests/test_cipher_hz2697.py
@@ -1,1 +1,8 @@
 from cipher_hz2697 import cipher_hz2697
+import pytest
+
+def test_cipher():
+    result = cipher_hz2697.cipher('love', shift = 1, encrypt = True)
+    expected = 'mpwf'
+    assert result == expected, 'result not equal to expected value'
+
